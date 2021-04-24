e8a3061d2fc4cf20fb32cb91dbe78365391bab32
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-04-24 18:48:12 -0500

    add function to avoid duplicate emails; add encription to passwords

--- src/api/Routes/auth.js
+++ src/api/Routes/auth.js
@@ -1,20 +1,29 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const { registerValidation } = require('../../validations');
 const User = require('../models/User');
 
 // VALIDATIONS
-
-
 
 router.post('/register', async (req, res) => {
   // VALIDATE DATA
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error);
 
+  // Check duplicates
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send({ error: 'this email is already chosen' });
+
+  // Encrypt passwords
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  // Create users
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
   try {
     const savedUser = await user.save();
