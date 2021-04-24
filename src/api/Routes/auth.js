6b32c5607d89e6f9fa946c3699e8118aafcce9d4
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-04-24 18:34:54 -0500

    create validation for registrations

--- src/api/Routes/auth.js
+++ src/api/Routes/auth.js
@@ -1,19 +1,15 @@
 const router = require('express').Router();
-const Joi = require('@hapi/joi');
+const { registerValidation } = require('../../validations');
 const User = require('../models/User');
 
 // VALIDATIONS
 
-const schema = {
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required(),
-};
+
 
 router.post('/register', async (req, res) => {
   // VALIDATE DATA
-  //const { error } = Joi.validate(req.body, schema);
-  //if (error) return res.status(400).send(error.details[0].message);
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error);
 
   const user = new User({
     name: req.body.name,
