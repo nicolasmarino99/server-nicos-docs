4935dc6c09c06e299e433e0e7454c320343ba398
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-04-24 19:28:00 -0500

    add jwt to response

--- src/api/Routes/auth.js
+++ src/api/Routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-const { registerValidation } = require('../../validations');
+const jwt = require('jsonwebtoken');
+const { registerValidation, loginValidation } = require('../../validations');
 const User = require('../models/User');
 
 // VALIDATIONS
@@ -27,10 +28,33 @@ router.post('/register', async (req, res) => {
   });
   try {
     const savedUser = await user.save();
-    res.send(savedUser);
+    // create and assign token
+    const token = await jwt.sign({ _id: savedUser._id }, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
   } catch (err) {
     res.status(400).send(err);
   }
+});
+
+router.post('/login', async (req, res) => {
+  // VALIDATE DATA
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error);
+
+  // Check if user exist
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (!emailExist) return res.status(400).send({ error: 'Email or password is wrong' });
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!emailExist) return res.status(400).send({ error: 'Email or password is wrong' });
+
+  // Validate password
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send({ error: 'Email or password is wrong' });
+
+  // create and assign token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header('auth-token', token).send(token);
 });
 
 module.exports = router;
